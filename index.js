60c1d6931eb90642e8a00c2126f384be0a7d07c7
awadh <awadh.hassan@student.moringaschool.com>
2022-08-07 11:45:29 +0300

    almost

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+
+ 
+
+
+const cards =["awadh","swaleh","omar","jaffer"];
+
+function writeCards(cards){
+    console.log(`Thank you , ${cards} for the wonderful birthday gift!`)
+}
+writeCards(cards[0]);
+writeCards(cards[1]);
+writeCards(cards[2]);
+writeCards(cards[3]);
+
+
+
+
+const count =["10","9","8","7","6","5","4","3","2","1","0"];
+
+function countDown(count){
+    let i = 0;
+    while(i < count.length){
+        console.log(`${count[i]}`);
+        i++;
+    }
+return count;
+}
+
+countDown(count);
